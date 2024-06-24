89cfd342f6fe24db47a1a68367ca7652f9733340
felix <felixzxy29@gmail.com>
2024-06-24 10:56:20 +0800

    finish login filter

---
+++ src/main/java/com/company/filter/LoginFilter.java
@@ -0,0 +1,39 @@
+package com.company.filter;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.Filter;
+
+public class LoginFilter implements Filter {
+    @Override
+    public void init (FilterConfig filterConfig) {}
+
+    @Override
+    public void doFilter (ServletRequest req, ServletResponse res, FilterChain chain) {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+        try {
+            request.setCharacterEncoding("UTF-8");
+            response.setCharacterEncoding("UTF-8");
+            if (request.getSession().getAttribute("sessionId") != null){
+                chain.doFilter(request, response);
+            } else {
+                System.out.println("please login!");
+                response.sendRedirect(request.getContextPath() + "/login.jsp");
+            }
+        } catch (IOException | ServletException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void destroy () {
+
+    }
+}
